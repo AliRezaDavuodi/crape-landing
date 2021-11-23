5d9a8ed78f7c356cc2832d90917767a7e78e729b
AliReza <AliRezaDavodiofficial@yahoo.com>
2021-11-23 09:03:39 +0330

    used dynamicly number for scrolling

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const arrowRight = document.querySelector(".arrow-right");
 const arrowLeft = document.querySelector(".arrow-left");
 const cards = document.querySelector(".cards");
+const cardsWidth = cards.getBoundingClientRect().width;
 
 const scrolling = function () {
   cards.scrollBy({
-    top: 0,
     left: this,
     behavior: "smooth",
   });
 };
-arrowRight.addEventListener("click", scrolling.bind(1000));
-arrowLeft.addEventListener("click", scrolling.bind(-1000));
+arrowRight.addEventListener("click", scrolling.bind(cardsWidth));
+arrowLeft.addEventListener("click", scrolling.bind(-cardsWidth));
